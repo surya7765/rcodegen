dd3b1cf06fd0091a46d0354445af05eb4a902563
Yugandhar Surya <suryayugandhar300@gmail.com>
2022-03-03 14:49:55 +0530

    first commit

--- users/apps.py
+++ users/apps.py
@@ -6,7 +6,7 @@ class UsersConfig(AppConfig):
     name = 'users'
 
     def ready(self):
-        from .signals import create_profile
+        import users.signals
         from django.db.models.signals import post_save
         from django.contrib.auth.models import User
         post_save.connect(create_profile, sender=User)
