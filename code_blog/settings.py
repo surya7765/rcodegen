bbe8d8476ebf003ff9e721bd82e79a0fc1c644b4
Yugandhar Surya <47697392+surya7765@users.noreply.github.com>
2022-12-27 14:30:28 +0530

    Update settings.py

--- code_blog/settings.py
+++ code_blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['rcodeg.herokuapp.com']
+ALLOWED_HOSTS = ['rcodeg.herokuapp.com','rcodegen.up.railway.app']
 
 # TinyMce
 TINYMCE_DEFAULT_CONFIG = {
